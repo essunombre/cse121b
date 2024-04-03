e1d18a9fdea66d5af28b8d64a4a2435523c96283
essunombre <alb21001@byui.edu>
2024-04-02 20:59:14 -0500

    first commit

---
+++ debugging/debugging.js
@@ -0,0 +1,21 @@
+// const pi = 3.14;
+// var radius = 3;
+// let area = 0;
+// area = radius * radius * pi;
+// console.log("Area 1: ", area)
+// radius = 4;
+// area = radius * radius * pi;
+// console.log("Area 2: ", area)
+
+// function circleArea(radius){
+//     return radius * radius * pi;
+// }
+
+
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  return radius * PI;
+}
+area = circleArea(3);
+console.log(area);
